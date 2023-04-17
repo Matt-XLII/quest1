76312086ec8aa003ab986df8be2cd5cdc92ae40c
Matt-XLII <durouchet.mathieu@gmail.com>
2023-04-17 10:59:15 +0200

    Creating the project and adding the basic ressources

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+
+const firstName= 'Matt'
+const age= 30
+const passions =['modelism', "video games", "climbing" ]
+
+console.log(firstName, age, passions)
+console.log(chalk.blue("Bryce"))
+console.log(chalk.green("Tophe"))
+console.log(chalk.red("Rachid"))
+console.log(chalk.yellow("Damien"))
